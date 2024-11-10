67c67f62ac4d02d1b29aa238cab9838d5fa78691
Chris Lowen <chrislowen@pm.me>
2024-11-10 08:20:07 -0500

    clean up how info is printed

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,24 @@ fn main() {
     let host = cpal::default_host();
     print_host_info(&host);
 
+    let default_input_device: Device = host
+        .default_output_device()
+        .expect("no default input device");
+
+    println!(
+        "\nDefault input device: {}",
+        default_input_device.name().expect("Device Name Error")
+    );
+
     let default_output_device: Device = host
         .default_output_device()
         .expect("no default output device");
 
-    println!("Default output device:");
-    print_device_info(&default_output_device);
+    println!(
+        "\nDefault output device: {}",
+        default_output_device.name().expect("Device Name Error")
+    );
 
-    let default_input_device: Device = host
-        .default_output_device()
-        .expect("no default input device");
-    println!("Default input device:");
-    print_device_info(&default_input_device);
     println!("\n\n\n---All Devices---");
 
     let all_devices = host.devices().expect("could not get all devices");
@@ -31,19 +37,25 @@ fn print_host_info(host: &cpal::Host) {
 }
 
 fn print_device_info(dev: &cpal::Device) {
-    println!(
-        "\n\n---Device: {}---",
-        dev.name().expect("Device Name Error")
-    );
-    let supported_input_configs = dev.supported_input_configs().unwrap();
-    let supported_output_configs = dev.supported_output_configs().unwrap();
-    println!("Supported Input Configs");
-    for c in supported_input_configs {
-        print_stream_config(&c)
+    println!("\n---Device: {}---", dev.name().expect("Device Name Error"));
+    let supported_input_configs: Vec<SupportedStreamConfigRange> =
+        dev.supported_input_configs().unwrap().collect();
+
+    let supported_output_configs: Vec<SupportedStreamConfigRange> =
+        dev.supported_output_configs().unwrap().collect();
+
+    if supported_input_configs.len() > 0 {
+        println!("Supported Input Configs");
+        for c in supported_input_configs {
+            print_stream_config(&c)
+        }
     }
-    println!("Supported Output Configs");
-    for c in supported_output_configs {
-        print_stream_config(&c)
+
+    if supported_output_configs.len() > 0 {
+        println!("\nSupported Output Configs");
+        for c in supported_output_configs {
+            print_stream_config(&c)
+        }
     }
 }
 
